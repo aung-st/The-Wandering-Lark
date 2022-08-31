42653cb5f9be891902321b266c67de93b5cf31c7
Sam Aung <92190177+aung-st@users.noreply.github.com>
2022-08-31 16:15:08 +0100

    Remove: Redundant controls

--- The-Wandering-Lark/Scenes/howToScene.py
+++ The-Wandering-Lark/Scenes/howToScene.py
@@ -9,17 +9,7 @@ class howToScene(Scene):
         self.add_object(Text((10,55), "A: Attack", font_size=35,align="left"))
         self.add_object(Text((10,90), "D: Defend", font_size=35,align="left"))
         self.add_object(Text((10,125), "R: Run", font_size=35,align="left"))
-        self.add_object(Text((10,160), "1: Use item 1", font_size=35,align="left"))
-        self.add_object(Text((10,195), "2: Use item 2", font_size=35,align="left"))
-        self.add_object(Text((10,230), "3: Use item 3", font_size=35,align="left"))
-        self.add_object(Text((10,265), "4: Use item 4", font_size=35,align="left"))
-
-        self.add_object(Text((400,20), "5: Use item 5", font_size=35,align="left"))
-        self.add_object(Text((400,55), "6: Use item 6", font_size=35,align="left"))
-        self.add_object(Text((400,90), "7: Use item 7", font_size=35,align="left"))
-        self.add_object(Text((400,125), "8: Use item 8", font_size=35,align="left"))
-        self.add_object(Text((400,160), "9: Use item 9", font_size=35,align="left"))
-        self.add_object(Text((400,195), "0: Use item 10", font_size=35,align="left"))
+        
 
 
     
