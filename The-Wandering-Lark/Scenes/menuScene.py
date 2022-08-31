a8122aa36d9ee28f457f5458d28a30a4dc4d5187
Sam Aung <92190177+aung-st@users.noreply.github.com>
2022-08-31 20:58:01 +0100

    Amend: menuScene code style formatting

--- The-Wandering-Lark/Scenes/menuScene.py
+++ The-Wandering-Lark/Scenes/menuScene.py
@@ -3,6 +3,7 @@ from .Objects import *
 
 class menuScene(Scene):
     def __init__(self, surface):
+        
         super().__init__(surface)
 
         self.add_object(Text((360,75), "MAIN MENU"))
