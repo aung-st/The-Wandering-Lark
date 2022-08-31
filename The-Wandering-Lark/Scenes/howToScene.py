9250758bc99b742a9bdeb5e857403588ddedced4
Sam Aung <92190177+aung-st@users.noreply.github.com>
2022-08-31 21:01:32 +0100

    Amend: code style formatting

--- The-Wandering-Lark/Scenes/howToScene.py
+++ The-Wandering-Lark/Scenes/howToScene.py
@@ -2,17 +2,15 @@ from .Scene import Scene
 from .Objects import *
 
 class howToScene(Scene):
+
     def __init__(self, surface):
+
         super().__init__(surface)
 
         self.add_object(Text((10,30), "Enter: Change Scenes", font_size=45,align="left"))
         self.add_object(Text((500,30), "A: Attack", font_size=45,align="left"))
         self.add_object(Text((10,90), "D: Defend", font_size=45,align="left"))
         self.add_object(Text((500,90), "R: Run", font_size=45,align="left"))
-        
-
-
-    
         
         self.add_object(Button((360,540), "Back", self.change_scene, "main menu"))    
 
