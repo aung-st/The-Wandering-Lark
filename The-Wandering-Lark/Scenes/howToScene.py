88db32ca5bfb5f663ba4ccad29eefdfa694f4aee
Sam Aung <92190177+aung-st@users.noreply.github.com>
2022-08-31 16:17:47 +0100

    Amend: How-to scene UI

--- The-Wandering-Lark/Scenes/howToScene.py
+++ The-Wandering-Lark/Scenes/howToScene.py
@@ -5,10 +5,10 @@ class howToScene(Scene):
     def __init__(self, surface):
         super().__init__(surface)
 
-        self.add_object(Text((10,20), "Enter: Change Scenes", font_size=35,align="left"))
-        self.add_object(Text((10,55), "A: Attack", font_size=35,align="left"))
-        self.add_object(Text((10,90), "D: Defend", font_size=35,align="left"))
-        self.add_object(Text((10,125), "R: Run", font_size=35,align="left"))
+        self.add_object(Text((10,30), "Enter: Change Scenes", font_size=45,align="left"))
+        self.add_object(Text((500,30), "A: Attack", font_size=45,align="left"))
+        self.add_object(Text((10,90), "D: Defend", font_size=45,align="left"))
+        self.add_object(Text((500,90), "R: Run", font_size=45,align="left"))
         
 
 
