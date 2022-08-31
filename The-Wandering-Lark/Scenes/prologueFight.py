87d1380ec1d4d54829db134f042d4ced75395e5f
Sam Aung <92190177+aung-st@users.noreply.github.com>
2022-08-31 21:06:54 +0100

    Amend: Code style formatting

--- The-Wandering-Lark/Scenes/prologueFight.py
+++ The-Wandering-Lark/Scenes/prologueFight.py
@@ -3,7 +3,9 @@ from .Objects import *
 
 
 class prologueFight(battleScene):
+
     def __init__(self, surface,total_enemies,player):
+        
         super().__init__(surface, total_enemies,player, "prologueEnd")
 
     
