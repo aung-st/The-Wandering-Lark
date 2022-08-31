0243b8382b1febe0825b75e9fba6e8e8af6d574c
Sam Aung <92190177+aung-st@users.noreply.github.com>
2022-08-31 21:06:25 +0100

    Amend: Code style formatting

--- The-Wandering-Lark/Scenes/patrol.py
+++ The-Wandering-Lark/Scenes/patrol.py
@@ -1,14 +1,19 @@
 from .battleScene import battleScene
 from .Objects import *
 import random
+
 class patrol(battleScene):
+
     def __init__(self, surface,total_enemies,player):
+
         super().__init__(surface, total_enemies,player,"caerham")
 
 
     def run(self):
+
         chance = random.choice(range(0, 10, 1))
         success = False
+        
         if chance >= 7: #30% chance of running
             success = True
             self.player_msg.set_text("")
